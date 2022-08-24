b74c98cf093966a876d5b03a1124dfeaf8869178
Jens Lorenz <jens.lorenz@zeiss.com>
2022-08-24 14:13:47 +0200

    feat: add example for using OIDC authentication with PiWeb API

---
+++ PiWeb.Api.Training.OidcAuth/LoginWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using Zeiss.PiWeb.Api.Rest.Common.Utilities;
+
+namespace PiWeb.Api.Training.OidcAuth
+{
+	/// <summary>
+	/// Interaction logic for LoginWindow.xaml
+	/// </summary>
+	public partial class LoginWindow : Window
+	{
+		private readonly string _ExtractInputsScriptTemplate;
+
+		public LoginWindow()
+		{
+			InitializeComponent();
+
+			var resourceName = typeof(LoginWindow).Namespace + ".Resources.extract_cloud_login_inputs.js";
+			using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
+				?? throw new NullReferenceException("missing script resource");
+			using (var streamReader = new StreamReader(stream, Encoding.UTF8))
+				_ExtractInputsScriptTemplate = streamReader.ReadToEnd();
+		}
+
+		public OAuthRequest? OAuthRequest { get; set; }
+		public OAuthResponse? OAuthResponse { get; set; }
+
+		private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if (DesignerProperties.GetIsInDesignMode(this))
+				return;
+
+			OAuthRequest = (OAuthRequest)e.NewValue;
+
+			if (OAuthRequest != null)
+				WebView.Source = new Uri(OAuthRequest.StartUrl);
+		}
+
+		private async void WebViewOnNavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+		{
+			if (OAuthRequest == null)
+				return;
+
+			// monitor the embedded browser until a form is displayed trying to post a response
+			// to the callback url
+			// extract all inputs from the form constructing a URL with all inputs as URL parameters
+			var extractInputsScript = _ExtractInputsScriptTemplate.Replace("%%CALLBACKURL%%", OAuthRequest.CallbackUrl);
+			var response = await WebView.CoreWebView2.ExecuteScriptAsync(extractInputsScript);
+			if (response == "null")
+				return;
+
+			// create the OAuthResponse using the URL from above
+			OAuthResponse = new OAuthResponse(response);
+
+			// close embedded browser successfully
+			DialogResult = true;
+			Close();
+		}
+	}
+}
---
+++ PiWeb.Api.Training.OidcAuth/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Windows;
+using Zeiss.PiWeb.Api.Rest.Common.Client;
+using Zeiss.PiWeb.Api.Rest.Common.Utilities;
+using Zeiss.PiWeb.Api.Rest.HttpClient.Data;
+
+namespace PiWeb.Api.Training.OidcAuth
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private async void ConnectButton_Click(object sender, RoutedEventArgs e)
+		{
+			// get the database id from the text box
+			bool isDatabaseIdValid = Guid.TryParse(DatabaseId.Text, out var databaseGuid);
+			if (!isDatabaseIdValid)
+			{
+				Output.Text = "invalid database id - please input the database id using the database url in PiWeb cloud portal";
+				return;
+			}
+
+			// combine database id with cloud base URI to get the server URI
+			var cloudBaseUri = new Uri("https://piwebcloud-service.metrology.zeiss.com/");
+			var serverUri = new Uri(cloudBaseUri, databaseGuid.ToString("D"));
+
+			// use OAuthHelper to get the OIDC access token for the service
+			var oAuthTokenCredential = OAuthHelper.GetAuthenticationInformationForDatabaseUrl(serverUri.AbsoluteUri, requestCallback: OAuthRequestCallback);
+
+			// create the REST client class and use the OIDC access token for authentication
+			var dataServiceRestClient = new DataServiceRestClient(serverUri);
+			dataServiceRestClient.AuthenticationContainer = new AuthenticationContainer(oAuthTokenCredential.AccessToken);
+
+			// invoke the service method querying data from PiWeb cloud database
+			var serviceInformation = await dataServiceRestClient.GetServiceInformation();
+
+			// display queried data
+			var serializedServiceInformation = JsonConvert.SerializeObject(serviceInformation, Formatting.Indented);
+			Output.Text = serializedServiceInformation;
+		}
+
+		private OAuthResponse? OAuthRequestCallback(OAuthRequest request)
+		{
+			// the OAuthRequest contains the start URL and callback URL
+			// display an embedded browser at the start URL
+			var loginWindow = new LoginWindow()
+			{
+				DataContext = request
+			};
+
+			var dialogResult = loginWindow.ShowDialog();
+
+			// if login was successful return the response
+			if (dialogResult == true)
+				return loginWindow.OAuthResponse;
+			else
+				return null;
+		}
+	}
+}
